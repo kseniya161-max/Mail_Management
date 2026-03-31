e55ce04a247def247ad95e46d3ce494a29f97934
Kseniya <baharevaxen@yandex.ru>
2026-03-31 13:50:13 +0300

    settings

--- config/urls.py
+++ config/urls.py
@@ -12,5 +12,12 @@ urlpatterns = [
     path('products/', include('products.urls', namespace='products')),
 ]
 
+from django.views.static import serve
+from django.urls import re_path
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
