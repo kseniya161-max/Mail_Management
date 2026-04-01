27357b5ea6fb170f528befa5851599bbeed96589
Kseniya <baharevaxen@yandex.ru>
2026-04-01 14:29:49 +0300

    correcting ProductViewset

--- products/api_views.py
+++ products/api_views.py
@@ -1,8 +1,6 @@
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet
 
 from products.models import Product, Category
 from products.serializers import ProductSerializer, CategorySerializer
@@ -13,6 +11,15 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated]
     authentication_classes = [SessionAuthentication, BasicAuthentication]
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        category_id = self.request.query_params.get('category')
+
+        if category_id:
+            queryset = queryset.filter(category_id=category_id)
+
+        return queryset
 
 
 class CategoryViewSet(ModelViewSet):
