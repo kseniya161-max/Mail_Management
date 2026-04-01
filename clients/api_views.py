8fad61b9f6cdd9e230d3b9508f510587257fccf8
Kseniya <baharevaxen@yandex.ru>
2026-04-01 14:39:42 +0300

    permissions for client

--- clients/api_views.py
+++ clients/api_views.py
@@ -3,17 +3,19 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from clients.models import Clients
+from clients.permissions import IsOwnerOrManager
 from clients.serializers import ClientSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 class ClientViewSet(ModelViewSet):
-    queryset = Clients.objects.all()
     serializer_class = ClientSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwnerOrManager]
     authentication_classes = [SessionAuthentication, BasicAuthentication]
 
     def get_queryset(self):
+        if self.request.user.role == 'manager':
+            return Clients.objects.all()
         return Clients.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
