55394dd195e97bab8d40893d268bba285e24a2ef
Kseniya <baharevaxen@yandex.ru>
2026-04-01 11:53:05 +0300

    create viewset for Clients

---
+++ clients/api_views.py
@@ -0,0 +1,19 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from clients.models import Clients
+from clients.serializers import ClientSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+class ClientViewSet(ModelViewSet):
+    queryset = Clients.objects.all()
+    serializer_class = ClientSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Clients.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(self, serializer)
+
--- clients/serializers.py
+++ clients/serializers.py
@@ -4,7 +4,8 @@ from rest_framework.serializers import ModelSerializer
 from clients.models import Clients
 
 
-class ClientSerializer(serializers, ModelSerializer):
+class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Clients
-        fields = ['user', 'email', 'name', 'comment', 'location']
+        fields = ['id','user', 'email', 'name', 'comment', 'location']
+
