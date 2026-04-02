6212a0aae97c6aa930edc85d23c1f25d614d4815
Kseniya <baharevaxen@yandex.ru>
2026-04-02 19:14:32 +0300

    correcting send_mail with attachment and services resend

--- clients/services.py
+++ clients/services.py
@@ -1,3 +1,6 @@
+import base64
+import os
+
 import resend
 from django.conf import settings
 import sib_api_v3_sdk
@@ -9,15 +12,28 @@ from io import BytesIO
 from clients.models import OfferFile
 
 
-def send_email_via_resend(to_email: str, subject: str, body: str):
+def send_email_via_resend(to_email: str, subject: str, body: str, file=None):
     resend.api_key = settings.RESEND_API_KEY
 
-    return resend.Emails.send({
+    params = {
         "from": settings.RESEND_FROM_EMAIL,
         "to": [to_email],
         "subject": subject,
         "text": body,
-    })
+    }
+
+    if file:
+        file.seek(0)
+        encoded_file = base64.b64encode(file.read()).decode("utf-8")
+
+        params["attachments"] = [
+            {
+                "filename": os.path.basename(file.name),
+                "content": encoded_file,
+            }
+        ]
+
+    return resend.Emails.send(params)
 
 def send_email_via_brevo(to_email: str, subject: str, body: str):
     configuration = sib_api_v3_sdk.Configuration()
--- clients/views.py
+++ clients/views.py
@@ -188,10 +188,15 @@ class MailingSendView(CreateView):
 
         for recipient in mailing.recipients.all():
             try:
+                attached_file = None
+                if mailing.message.offer_file and mailing.message.offer_file.file:
+                    attached_file = mailing.message.offer_file.file
+
                 response = send_email_via_resend(
                     to_email=recipient.email,
                     subject=mailing.message.header,
                     body=mailing.message.content,
+                    file=attached_file,
                 )
 
                 MailingAttempt.objects.create(
@@ -218,51 +223,7 @@ class MailingSendView(CreateView):
             }
         )
 
-    # def send_mailing(self, mailing):
-    #     success_count = 0
-    #     failed_count = 0
-    #
-    #     for recipient in mailing.recipients.all():
-    #         try:
-    #             sent_count = send_mail(
-    #                 mailing.message.header,
-    #                 mailing.message.content,
-    #                 settings.DEFAULT_FROM_EMAIL,
-    #                 [recipient.email],
-    #                 fail_silently=False,
-    #             )
-    #
-    #             if sent_count == 1:
-    #                 MailingAttempt.objects.create(
-    #                     mailing=mailing,
-    #                     status='success',
-    #                     server_response=f'SMTP accepted message for {recipient.email}',
-    #                 )
-    #                 success_count += 1
-    #             else:
-    #                 MailingAttempt.objects.create(
-    #                     mailing=mailing,
-    #                     status='failed',
-    #                     server_response=f'SMTP did not accept message for {recipient.email}',
-    #                 )
-    #                 failed_count += 1
-    #
-    #         except Exception as e:
-    #             MailingAttempt.objects.create(
-    #                 mailing=mailing,
-    #                 status='failed',
-    #                 server_response=str(e),
-    #             )
-    #             failed_count += 1
-    #
-    #     EmailStatistics.objects.update_or_create(
-    #         user=self.request.user,
-    #         mailing=mailing,
-    #         defaults={
-    #             'success_attempt_mailing': success_count,
-    #             'failed_attempt_mailing': failed_count,
-    #         }
-    #     )
+
 
 @method_decorator(cache_page(60 * 3), name='dispatch')
 class HomePageView(TemplateView):
