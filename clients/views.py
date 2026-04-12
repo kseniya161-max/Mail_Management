995126a86dab56c0376a4cc6548fb98a3be21773
Kseniya <baharevaxen@yandex.ru>
2026-04-12 14:13:07 +0300

    correct form for message and view

--- clients/views.py
+++ clients/views.py
@@ -98,6 +98,11 @@ class MessageCreateView(LoginRequiredMixin, CreateView):
     template_name = "message_create.html"
     success_url = reverse_lazy("clients:message_list")
 
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs["user"] = self.request.user
+        return kwargs
+
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
