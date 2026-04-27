ff8b0238c04882e5c9a7bc01d0fc1df6416b6e88
Kseniya <baharevaxen@yandex.ru>
2026-04-27 14:48:37 +0300

    creating ClientOfferFilesView

--- documents/views.py
+++ documents/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
+from django.views.generic import ListView
+
 from clients.forms import OfferFileForm
-from clients.models import Clients
+from clients.models import Clients, OfferFile
 from clients.services.file_service import generate_offer_file
 from django.contrib import messages
 
@@ -51,3 +53,32 @@ class ClientOfferFileCreateView(LoginRequiredMixin, View):
                 "client": client,
             },
         )
+
+
+class ClientOfferFilesView(LoginRequiredMixin, ListView):
+    template_name = "client_offer_files.html"
+    model = OfferFile
+    context_object_name = "client_offer_files"
+
+    def get_client(self):
+        queryset = Clients.objects.all()
+
+        if self.request.user.role != "manager":
+            queryset = queryset.filter(user=self.request.user)
+
+        return get_object_or_404(queryset, pk=self.kwargs["pk"])
+
+    def get_queryset(self):
+        self.client = self.get_client()
+
+        queryset = OfferFile.objects.filter(client=self.client)
+
+        if self.request.user.role != "manager":
+            queryset = queryset.filter(created_by=self.request.user)
+
+        return queryset.order_by("-created_at")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["client"] = self.client
+        return context
