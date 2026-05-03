53ea064a80c4d2c96b1003e89516f4ddfb5b4b0f
Kseniya <baharevaxen@yandex.ru>
2026-05-03 22:21:54 +0300

    repeat

--- config/settings.py
+++ config/settings.py
@@ -191,7 +191,7 @@ if os.getenv("DATABASE_URL"):
         "default": dj_database_url.config(
             default=os.getenv("DATABASE_URL"),
             conn_max_age=600,
-            ssl_require=not DEBUG,
+            ssl_require=os.getenv("DB_SSL", "False") == "True",
         )
     }
 else:
