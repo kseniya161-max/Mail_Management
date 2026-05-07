fbd700b8feb9b4708c4e67a8b73a876b2bea73d9
Kseniya <baharevaxen@yandex.ru>
2026-05-07 17:53:42 +0300

    number invoice unique

--- documents/forms.py
+++ documents/forms.py
@@ -19,6 +19,14 @@ class InvoiceForm(forms.ModelForm):
             )
         }
 
+    def clean_number(self):
+        number = self.cleaned_data["number"]
+        if Invoice.objects.filter(number=number).exists():
+            raise forms.ValidationError('Счет с таким номером существует')
+        return number
+
+
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields["number"].required = True
--- documents/models.py
+++ documents/models.py
@@ -14,7 +14,7 @@ class Invoice(models.Model):
     created_by = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="invoices"
     )
-    number = models.CharField(max_length=50, blank=True)
+    number = models.CharField(max_length=50, unique=True)
     file = models.FileField(upload_to="invoices/", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     vat_rate = models.DecimalField(
