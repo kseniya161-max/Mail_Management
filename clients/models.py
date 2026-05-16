738f66652ea9e1c0be73e270ff09ffa1db3daec6
Kseniya <baharevaxen@yandex.ru>
2026-05-16 16:59:01 +0300

    remove permissions from models

--- clients/models.py
+++ clients/models.py
@@ -41,11 +41,6 @@ class Clients(models.Model):
     def __str__(self):
         return self.name
 
-    class Meta:
-        permissions = [
-            ("can_manage_clients", "Can manage clients"),
-        ]
-
 
 class Message(models.Model):
     user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
@@ -68,11 +63,6 @@ class Message(models.Model):
 
     def __str__(self):
         return self.header
-
-    class Meta:
-        permissions = [
-            ("can_manage_message", "Can manage message"),
-        ]
 
 
 def get_default_end_date():
@@ -99,11 +89,6 @@ class Mailing(models.Model):
     def __str__(self):
         return f"Рассылка: {self.message.header}  - Статус: {self.get_status_display()}"
 
-    class Meta:
-        permissions = [
-            ("can_manage_mailing", "Can manage mailing"),
-        ]
-
 
 class MailingAttempt(models.Model):
     """Модель попытки рассылок"""
@@ -120,11 +105,6 @@ class MailingAttempt(models.Model):
 
     def __str__(self):
         return f"Попытка рассылки: {self.status} - {self.attempt_time}"
-
-    class Meta:
-        permissions = [
-            ("can_manage_mailing", "Can manage mailing"),
-        ]
 
 
 class EmailStatistics(models.Model):
